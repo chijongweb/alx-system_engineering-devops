1fe2bed0071443283cd949042502ea218af5ff46
chijongweb <160375788+chijongweb@users.noreply.github.com>
2025-06-30 08:28:14 +0200

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,23 +1,13 @@
-#!/usr/bin/env bash
-# This Puppet script configures the SSH client to use a specific private key and disable password authentication
-
-file { '/home/vagrant/.ssh/config':
-  ensure => 'file',
-  owner  => 'vagrant',
-  group  => 'vagrant',
-  mode   => '0600',
-}
+# Configure /etc/ssh/ssh_config to use a specific private key and disable password authentication
 
 file_line { 'Declare identity file':
-  path  => '/home/vagrant/.ssh/config',
-  line  => '  IdentityFile ~/.ssh/school',
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/school',
   match => '^\\s*IdentityFile',
-  require => File['/home/vagrant/.ssh/config'],
 }
 
 file_line { 'Turn off passwd auth':
-  path  => '/home/vagrant/.ssh/config',
-  line  => '  PasswordAuthentication no',
+  path  => '/etc/ssh/ssh_config',
+  line  => '    PasswordAuthentication no',
   match => '^\\s*PasswordAuthentication',
-  require => File['/home/vagrant/.ssh/config'],
 }
